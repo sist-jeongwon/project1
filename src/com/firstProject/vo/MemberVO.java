f7cc78c37930e45a9fe34fbb4811cc184e07231e
Administrator <Administrator@WIN-R8T1UH8D9UT>
2020-10-21 17:34:48 +0900

    11

---
+++ src/com/firstProject/vo/MemberVO.java
@@ -0,0 +1,65 @@
+package com.firstProject.vo;
+
+import java.util.*;
+
+public class MemberVO {
+
+	private String mem_id;
+	private String name;
+	private String mem_pwd;
+	private Date birth;
+	private String email;
+	private String adderess;
+	private String tel;
+	private int admin;
+	
+	public String getMem_id() {
+		return mem_id;
+	}
+	public void setMem_id(String mem_id) {
+		this.mem_id = mem_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMem_pwd() {
+		return mem_pwd;
+	}
+	public void setMem_pwd(String mem_pwd) {
+		this.mem_pwd = mem_pwd;
+	}
+	public Date getBirth() {
+		return birth;
+	}
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAdderess() {
+		return adderess;
+	}
+	public void setAdderess(String adderess) {
+		this.adderess = adderess;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public int getAdmin() {
+		return admin;
+	}
+	public void setAdmin(int admin) {
+		this.admin = admin;
+	}
+
+}
