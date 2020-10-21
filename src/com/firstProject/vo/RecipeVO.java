a68b3e92e7e671083e2bcee17473b9564fa84ffc
SIST <SIST@DESKTOP-I8FQNHB>
2020-10-21 16:42:41 +0900

    RecipeVO

--- src/com/firstProject/vo/RecipeVO.java
+++ src/com/firstProject/vo/RecipeVO.java
@@ -1,5 +1,63 @@
 package com.firstProject.vo;
 
-public class RecipeVO {
+import java.util.Date;
 
+public class RecipeVO {
+	private int recipe_no;
+	private String title;
+	private Date date;
+	private int hit;
+	private String poster;
+	private String content;
+	private String ingredient;
+	private String mem_id;
+	public int getRecipe_no() {
+		return recipe_no;
+	}
+	public void setRecipe_no(int recipe_no) {
+		this.recipe_no = recipe_no;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public int getHit() {
+		return hit;
+	}
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+	public String getPoster() {
+		return poster;
+	}
+	public void setPoster(String poster) {
+		this.poster = poster;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getIngredient() {
+		return ingredient;
+	}
+	public void setIngredient(String ingredient) {
+		this.ingredient = ingredient;
+	}
+	public String getMem_id() {
+		return mem_id;
+	}
+	public void setMem_id(String mem_id) {
+		this.mem_id = mem_id;
+	}
+	
 }
