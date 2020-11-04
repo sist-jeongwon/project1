1165e7d9d5dc90ac63392df6c93cddda4611eebd
yutiti <wjddnjs4002@naver.com>
2020-11-04 22:18:18 +0900

    home 화면(미완성)

--- WebContent/main/home.jsp
+++ WebContent/main/home.jsp
@@ -13,6 +13,45 @@
 	padding: 0;
 }
 
+.main-section {
+	width: 1140px;
+	position: relative;
+	margin: auto;
+}
+
+.main-section p {
+	font-size: 17px;
+}
+
+.main-section img {
+	margin: 20px;
+	max-width: 55%;
+}
+
+.main-section p strong {
+	font-size: 1.2em;
+	color: rgb(112, 173, 71);
+}
+
+.main-section .problems ul {
+	padding: 10px;
+	height: 500px;
+	position: relative;
+	margin: auto;
+}
+
+.main-section .problems ul li img {
+	max-width: 50%;
+}
+
+#sec1 {
+	height: 350px;
+}
+
+#sec2 {
+	height: 1000px;
+}
+
 .slide {
 	width: 1140px;
 	height: 350px;
@@ -31,24 +70,30 @@
 
 .slide ul li {
 	display: inline-block;
-	margin: 20px;
+	margin: 20px !important;
 }
+
 .post-title-main {
-        display: block;
-        font-size: 15px;
-        margin-bottom: 0;
-        line-height: 1.1;
-        -webkit-transition-duration: 500ms;
-        transition-duration: 500ms; }
+	display: block;
+	font-size: 15px;
+	margin-bottom: 0;
+	line-height: 1.1;
+	-webkit-transition-duration: 500ms;
+	transition-duration: 500ms;
+}
+
 #back {
+	color: white;
 	position: absolute;
 	top: 150px;
 	left: 0;
 	cursor: pointer;
 	z-index: 10;
+	position: absolute;
 }
 
 #next {
+	color: white;
 	position: absolute;
 	top: 150px;
 	right: 0;
@@ -58,14 +103,63 @@
 </style>
 </head>
 <body>
+	<div class="main-section" id="sec1">
+		<div class="text-center">
+			<img src="../style/img/core-img/section_title_1.png"
+				alt="채소 한끼, 최소 한끼로 지구를 지켜주세요!">
+		</div>
+		<div>
+			<p>
+				<strong>육식으로 인해 기후변화가 가속화되고 있다는 것을 알고 계셨나요?</strong> <br>육류 생산
+				과정에서 발생하는 탄소 배출은 모든 교통수단을 합친 것보다 많습니다. <br>고기 소비가 계속 늘어나면서 숲이
+				파괴되고 토양과 물, 대기 오염문제까지 발생하고 있으며, 대규모 공장식 가축 사육으로 동물들은 고통받고 있습니다.
+			</p>
+		</div>
+	</div>
+	<div class="main-section" id="sec2">
+		<div class="text-center">
+			<img src="../style/img/core-img/section_title_2.png"
+				alt="육식에 대한 불편한 진실">
+		</div>
+		<div>
+			<p>공장식 축산 과정에서 전염병을 막기 위해 많은 양의 항생제가 사용됩니다. 농장에서 사육되는 동물들은 살충제와
+				화학비료로 키운 사료를 먹습니다. 대규모 공장식 축산은 탄소배출이나 환경오염 등의 문제를 일으키는 것은 물론, 우리의
+				건강을 위협하고 있습니다.</p>
+		</div>
+		<div class="contaimer">
+			<div class="row">
+				<div class="problems col-12 col-sm-6 col-lg-4">
+					<ul>
+						<li><img src="../style/img/core-img/problem1.png">
+							<p>육류 과다 섭취는 당뇨, 동맥경화, 심혈관 질환은 물론 암 등 치명적인 질병의 원인이 되기도 합니다.
+								식물성 위주의 식단은 제2형 당뇨병 발생 위험을 40% 줄일 수 있다고 합니다.</p></li>
+						<li><img src="../style/img/core-img/problem2.png">
+							<p>1960년에서 2011년까지 50년 동안, 전세계 전환된 토지의 65%가 축산업을 위한 개간이었습니다.
+								계속해서 늘어나는 가축들의 사료를 마련하기 위해, 지금도 생물다양성이 풍부한 숲이 베어져나가고 있습니다.</p></li>
+						<li><img src="../style/img/core-img/problem3.png">
+							<p>대규모 산업식 축산은 이윤추구를 위해 가능한 한 빨리 동물을 키우고 도살하는 시스템을 만들었습니다. 이로
+								인해 동물들은 살아있는 동안은 엄청난 고통을 겪다 무참히 도살 당하고 있습니다.</p></li>
+						<li><img src="../style/img/core-img/problem4.png">
+							<p>현재 축산업으로 인한 온실가스 배출량은 전체의 14%를 차지하고 있습니다. 이대로 방치한다면
+								2050년까지 육식으로 인한 탄소배출량은 전체 배출량의 절반 이상을 차지하게 될 것입니다.</p></li>
+						<li><img src="../style/img/core-img/problem5.png">
+							<p>가축 사육은 엄청난 양의 물을 사용할 뿐 아니라 수질 오염의 원인 중 하나입니다. 축산업으로 인한 물
+								사용은 전체 농업에서 사용하는 양의 29%를 차지합니다. 대규모 산업식 축산 농장에서 사용하는 화학비료, 살충제,
+								가축을 위한 약품들은 물과 토양을 오염시키고 결국 우리의 식탁으로 돌아와 건강을 위협합니다.</p></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+
 	<div class="slide">
-		<div class="post-catagory section-padding-100">
+		<div class="post-catagory">
 			<img id="back" src="../style/img/core-img/left.png" alt="" width="50">
 			<div class="container">
 				<ul>
 					<c:forEach var="vo" items="${list }">
 						<li>
-							<div class="single-post-catagory mb-30">
+							<div class="single-post-catagory">
 								<img src="${vo.poster }" alt=""
 									style="width: 300px; height: 300px;">
 								<div class="catagory-content-bg">
@@ -89,7 +183,8 @@
 					</c:forEach>
 				</ul>
 			</div>
-			<img id="next" src="../style/img/core-img/right.png" alt="" width="50">
+			<img id="next" src="../style/img/core-img/right.png" alt=""
+				width="50">
 		</div>
 	</div>
 </body>
@@ -104,14 +199,14 @@
 
 		//버튼을 클릭했을 때 함수 실행
 		$('#back').click(function() {
-			if (img_position <= 1)
-				end();
+			if (img_position < 1)
+				stop();
 			else
 				back();
 		});
 		$('#next').click(function() {
-			if (img_position >= img_count)
-				start();
+			if (img_position > (img_count - 3))
+				stop();
 			else
 				next();
 		});
@@ -119,26 +214,21 @@
 		function back() {
 			if (1 < img_position) {
 				imgs.animate({
-					left : '+=340px'
+					left : '+=1020px'
 				});
-				img_position--;
+				img_position -= 4;
 			}
 		}
 		function next() {
 			if (img_count > img_position) {
 				imgs.animate({
-					left : '-=340px'
+					left : '-=1020px'
 				});
-				img_position++;
+				img_position += 4;
 			}
 		}
 		//이미지 끝까지 가면 버튼 사라지기
-		//첫 이미지로 돌아오기
-		function end() {
-			img.animate
-		}
-		function start() {
-
+		function stop() {
 		}
 	});
 </script>
--- src/com/firstProject/model/RestaurantModel.java
+++ src/com/firstProject/model/RestaurantModel.java
@@ -1,6 +1,5 @@
 package com.firstProject.model;
 
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,130 +10,114 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
 import com.firstProject.dao.RestaurantDAO;
 import com.firstProject.vo.LikeVO;
 import com.firstProject.vo.RestaurantVO;
 import com.firstProject.vo.Restaurant_reviewVO;
 import com.sist.controller.RequestMapping;
 
-
-
-
 public class RestaurantModel {
 
-	  @RequestMapping("restaurant/restaurant_main.do")
-	  public String restaurant_main(HttpServletRequest request)
-	   {
+	@RequestMapping("restaurant/restaurant_main.do")
+	public String restaurant_main(HttpServletRequest request) {
 		// 데이터 읽어서 => jsp 전송(데이터베이스)
-		   // 페이지 
-		   String page=request.getParameter("page");
-		   if(page==null)
-			   page="1";
-		   int curpage=Integer.parseInt(page);
-		   Map map=new HashMap();
-		   int rowSize=12;
-		   int start=(rowSize*curpage)-(rowSize-1);
-		   int end=rowSize*curpage;
-		   map.put("start", start);
-		   map.put("end",end);
-		   List<RestaurantVO> list=RestaurantDAO.restaurantListData(map);
-		   int totalpage=RestaurantDAO.restaurantTotalPage();
-		   
-		   int BLOCK=5;
-		   int startPage=((curpage-1)/BLOCK*BLOCK)+1;
-		   int endPage=((curpage-1)/BLOCK*BLOCK)+BLOCK;
-		   if(endPage>totalpage)
-			   endPage=totalpage;
-		   request.setAttribute("list", list);
-		   request.setAttribute("curpage", curpage);
-		   request.setAttribute("totalpage", totalpage);
-		   request.setAttribute("BLOCK", BLOCK);
-		   request.setAttribute("startPage", startPage);
-		   request.setAttribute("endPage", endPage);
-		   request.setAttribute("main_jsp", "../restaurant/restaurant_main.jsp");
-		   
-		   HttpSession session=request.getSession();
-			String id=(String)session.getAttribute("id");
+		// 페이지
+		String page = request.getParameter("page");
+		if (page == null)
+			page = "1";
+		int curpage = Integer.parseInt(page);
+		Map map = new HashMap();
+		int rowSize = 12;
+		int start = (rowSize * curpage) - (rowSize - 1);
+		int end = rowSize * curpage;
+		map.put("start", start);
+		map.put("end", end);
+		List<RestaurantVO> list = RestaurantDAO.restaurantListData(map);
+		int totalpage = RestaurantDAO.restaurantTotalPage();
 
-			// 쿠키 읽기
-			Cookie[] cookies=request.getCookies();
-			List<RestaurantVO> cList=new ArrayList<RestaurantVO>();
-			if(cookies!=null)
-			{
-				for(int i=0;i<cookies.length;i++)
-				{
-					if(cookies[i].getName().startsWith(id))
-					{
-						String no=cookies[i].getValue();
-						if(no == null || no.trim().equals(""))
-						{
-							no= "0";
-						}
-						RestaurantVO vo=RestaurantDAO.restaurantDetailData(Integer.parseInt(no));
-						cList.add(vo);
+		int BLOCK = 5;
+		int startPage = ((curpage - 1) / BLOCK * BLOCK) + 1;
+		int endPage = ((curpage - 1) / BLOCK * BLOCK) + BLOCK;
+		if (endPage > totalpage)
+			endPage = totalpage;
+		request.setAttribute("list", list);
+		request.setAttribute("curpage", curpage);
+		request.setAttribute("totalpage", totalpage);
+		request.setAttribute("BLOCK", BLOCK);
+		request.setAttribute("startPage", startPage);
+		request.setAttribute("endPage", endPage);
+		request.setAttribute("main_jsp", "../restaurant/restaurant_main.jsp");
+
+		HttpSession session = request.getSession();
+		String id = (String) session.getAttribute("id");
+
+		// 쿠키 읽기
+		Cookie[] cookies = request.getCookies();
+		List<RestaurantVO> cList = new ArrayList<RestaurantVO>();
+		if (cookies != null) {
+			for (int i = 0; i < cookies.length; i++) {
+				if (cookies[i].getName().startsWith(id)) {
+					String no = cookies[i].getValue();
+					if (no == null || no.trim().equals("")) {
+						no = "0";
 					}
+					RestaurantVO vo = RestaurantDAO.restaurantDetailData(Integer.parseInt(no));
+					cList.add(vo);
 				}
 			}
-			request.setAttribute("cList", cList);
-		   return "../main/main.jsp";
-	   }
-	  
-	
+		}
+		request.setAttribute("cList", cList);
+		return "../main/main.jsp";
+	}
 
-		  @RequestMapping("restaurant/restaurant_detail_before.do") // 쿠키 전송 받는다 => 요청 을 detail로 다시 보낸다
-			
-			public String restaurant_detail_before(HttpServletRequest request, HttpServletResponse response) 
-		    { 
-			  String no = request.getParameter("rest_no");
-			    HttpSession session=request.getSession();
-				String id=(String)session.getAttribute("id");
-				Cookie cookie=new Cookie(id+no, no);
-			
-				cookie.setMaxAge(60*60*24);
-				
-				//전송
-				response.addCookie(cookie);
+	@RequestMapping("restaurant/restaurant_detail_before.do") // 쿠키 전송 받는다 => 요청 을 detail로 다시 보낸다
 
-				//cookie=new Cookie("p"+no, no);
-				return "redirect:../restaurant/restaurant_detail.do?rest_no="+no;// 쿠키값을 저장한 채로 재요청 (detail로 이동)
-			}
+	public String restaurant_detail_before(HttpServletRequest request, HttpServletResponse response) {
+		String no = request.getParameter("rest_no");
+		HttpSession session = request.getSession();
+		String id = (String) session.getAttribute("id");
+		Cookie cookie = new Cookie(id + no, no);
 
-			@RequestMapping("restaurant/restaurant_detail.do")
-			public String restaurnat_detail(HttpServletRequest request)
-			{
-				String no=request.getParameter("rest_no");
-				   
-				 RestaurantVO vo=RestaurantDAO.restaurantDetailData(Integer.parseInt(no));
-				
-				
-				request.setAttribute("vo", vo);
-				request.setAttribute("main_jsp", "../restaurant/restaurant_detail.jsp");
-				
-				return "../main/main.jsp";
-			}
-		 
-		  
-		  ////////////////////////////////////////////////////////////
-			// 찜
-					@RequestMapping("restaurant/like.do")
-					public String restaurant_like(HttpServletRequest request)
-					{
-						String no=request.getParameter("no");
-						HttpSession session=request.getSession();
-						String id=(String)session.getAttribute("id");
-						LikeVO vo=new LikeVO();
-						vo.setMem_id(id);
-						vo.setCno(Integer.parseInt(no));
-						RestaurantDAO.likeInsert(vo);
-						return "redirect:../restaurant/detail.do?no="+no;
-					}
-					@RequestMapping("restaurant/like_cancel.do")
-					public String like_cancel(HttpServletRequest request)
-					{
-						String no=request.getParameter("no");
-						RestaurantDAO.likeDelete(Integer.parseInt(no));
-						return "redirect:../reserve/mypage.do";
-					}		 
-	 
+		cookie.setMaxAge(60 * 60 * 24);
+
+		// 전송
+		response.addCookie(cookie);
+
+		// cookie=new Cookie("p"+no, no);
+		return "redirect:../restaurant/restaurant_detail.do?rest_no=" + no;// 쿠키값을 저장한 채로 재요청 (detail로 이동)
+	}
+
+	@RequestMapping("restaurant/restaurant_detail.do")
+	public String restaurnat_detail(HttpServletRequest request) {
+		String no = request.getParameter("rest_no");
+
+		RestaurantVO vo = RestaurantDAO.restaurantDetailData(Integer.parseInt(no));
+
+		request.setAttribute("vo", vo);
+		request.setAttribute("main_jsp", "../restaurant/restaurant_detail.jsp");
+
+		return "../main/main.jsp";
+	}
+
+	////////////////////////////////////////////////////////////
+	// 찜
+	@RequestMapping("restaurant/like.do")
+	public String restaurant_like(HttpServletRequest request) {
+		String no = request.getParameter("no");
+		HttpSession session = request.getSession();
+		String id = (String) session.getAttribute("id");
+		LikeVO vo = new LikeVO();
+		vo.setMem_id(id);
+		vo.setCno(Integer.parseInt(no));
+		RestaurantDAO.likeInsert(vo);
+		return "redirect:../restaurant/detail.do?no=" + no;
+	}
+
+	@RequestMapping("restaurant/like_cancel.do")
+	public String like_cancel(HttpServletRequest request) {
+		String no = request.getParameter("no");
+		RestaurantDAO.likeDelete(Integer.parseInt(no));
+		return "redirect:../reserve/mypage.do";
+	}
+
 }
